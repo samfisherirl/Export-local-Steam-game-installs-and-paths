e759022ae0af7497ba9588ded4656caa218b57b5
sam fisher <98753696+samfisherirl@users.noreply.github.com>
2022-12-17 17:38:22 -0600

    Add files via upload

--- VParse.py
+++ VParse.py
@@ -4,47 +4,240 @@ import os
 import sys
 import shutil
 import json
+import os.path
+import library_paths as LP
 
 appinfo = r'C:\Program Files (x86)\Steam\appcache\appinfo.vdf'
 
+
 def moveV(directory):
-    shutil.copy2(appinfo, (os.path.join(directory, 'appinfo.vdf')))
+    global appinfo
+    try:
+        shutil.copy2(appinfo, (os.path.join(directory, 'appinfo.vdf')))
+        return True
+
+    except:
+        print(
+            'file not found in \'C:\Program Files (x86)\Steam\appcache\appinfo.vdf\'\nTry placing it in the same directory as this application.\nIf you already have, ignore this error.')
+    try:
+        if os.path.exists(os.path.join(directory, 'appinfo.vdf')):
+            print('\n\nFound it!')
+            appinfo = os.path.join(directory, 'appinfo.vdf')
+            return True
+    except:
+        return False
+
+
+class Library:
+    def __init__(self, exe, path, name, longpath):
+        self.exe = str(exe)
+        self.path = str(path)
+        self.name = str(name)
+        self.longpath = str(longpath)
+
+    # exampled output times 3k 
+    #  longpath: 'C:\\program files\\Steam\\common\\\\Half-Life\\hlds.exe'
+    #   exe:  'hlds.exe'
+    #   name: 'Half-Life Dedicated Server'
+    #
+
+
+def constructor(gameLib):
+    # create library objects,
+    # using a list to keep track
+    lib = []
+    x = 0
+    for i in gameLib:
+        lib.append(Library(i['exe'], i['path'], i['name'], 'xxxxx'))
+    x = x + 1
+    return lib
+
+
+def callLibrary(lib):
+    pathers = LP.main()
+    print(str(pathers))
+    print(lib[3].path + lib[3].exe)
+    return pathers
+
+
+def pathValidator(paths, lib):
+    global matched
+    matched = []
+    for i in lib:
+        for path in paths:
+            struct = i.path  # + "\\" + i.exe
+            post = i.path + "\\" + i.exe
+            x = path + struct.replace('\\\\', '\\')
+            if os.path.exists(x):
+                i.longpath = x + "\\" + i.exe
+                matched.append(i.longpath)
+    print('finished')
+    print(str(matched))
+    return [lib, matched]
+    # exampled output times 1k 
+    #  path: 'C:\\program files\\Steam\\# It's a string.
+    # common\\\\Half-Life\\hlds.exe'
+    #   exe:  'hlds.exe'
+    #   name: 'Half-Life Dedicated Server'
+    #
 
 
 # f'{VDFexe} {appinfo} --pretty >output.json'
 def cmd4(directory):
-# Joining the directory and the VDFP.exe file.
+    # Joining the directory and the VDFP.exe file.
     VDFexe = os.path.join(directory, 'VDFP.exe')
     out = os.path.join(directory, 'output.json')
-    
-    result = subprocess.run([VDFexe, appinfo, '-p'], capture_output=True) # , "--pretty", ">output.json"
-# It's converting the output of the command to a string.
+
+    result = subprocess.run([VDFexe, appinfo, '-p'], capture_output=True)  # , "--pretty", ">output.json"
+    # It's converting the output of the command to a string.
     val = str(result.stdout.strip(), encoding='utf-8')
 
     vals = json.loads(val)
     list = vals['datasets']
     return list
-    
-def parse(list):
-    array = []
+
+
+def parser(list):
+    global gameLib
+    gameLib = []
+    game = {}
+    # data format
+    # list of games
+    # games have attributes (dict)
+    # this separates games, dupes and vals
+    dot = '.'
+    x = 0
+    iterator = ['name', 'path', 'exe', 'longpath']
+
     for i in list:
+        x = x + 1
         try:
-            array.append(i['data']['appinfo']['config']['launch']['0']) 
+            exe = i['data']['appinfo']['config']['launch']
         except:
             continue
+        try:
+            wd = i['data']['appinfo']['config']['installdir']
+            wd.replace('/', '\\')
+            name = i['data']['appinfo']['common']['name']
+            name.replace('/', '\\')
+        except:
+            continue
+        z = 0
+        for i, z in enumerate(exe.items()):
+            try:
+                executable = exe[str(i)]['executable']
+                executable = executable.replace('/', '\\')
+            except:
+                continue
+            if ".exe" in executable:
+                p = 'C:\\program files\\Steam\common\\'
+                holder = [
+                    wd,
+                    name,
+                    executable,
+                    'xxxxxx'
+                ]
 
+                for t, y in zip(iterator, holder):
+                    try:
+                        # game.update({f"{i}": f"{y}" })
+                        game.update({f'{t}': f'{y}'})
+                        # writer(iterator, holder, x)
+                    except:
+                        break
+
+                gameLib.append(game)
+                # writer(iterator, holder, x)
+                game = {}
+            # exes = exe['0']['executable']
+            # holder = [wd, name, i['executable']]
+            else:
+                continue
+            dot = dot + '.'
+    return gameLib
+
+    def user():
+        # ################################################
+        # x = 0
+        # for i in list:
+        #     x = x + 1
+        #     iterator = ['name', 'path', 'exe', 'longpath']
+        #     try:
+        #         exe = i['data']['appinfo']['config']['launch']
+        #     except:
+        #         continue
+        #     try:
+        #         wd =  i['data']['appinfo']['config']
+        #         name = i['data']['appinfo']['common']['name']
+        # ['executable']:
+        #                 p = 'C:\\program files\\Steam\common\\'
+        #                 batch = p + "\\" + wd['installdir'] + "\\" + exe[str(z)]['executable']
+        #                 holder = [
+
+        #                             ]
+
+        #                 for t, y in zip(iterator, holder):
+        #                     try:
+        #                         #game.update({f"{i}": f"{y}" })
+        #                         game.update({f'{t}': f'{y}'})
+        #                         #writer(iterator, holder, x)
+        #                     except:
+        #                         break
+
+        #                 gameLib.append(game)
+        #                 #writer(iterator, holder, x)
+        #                 z = z + 1
+        #                 game = {}
+        #         # exes = exe['0']['executable']
+        #         # holder = [wd, name, i['executable']]
+        #     except:
+        #         print('matched on everything but there was an error on writer')
+        # return gameLib
+        pass
+
+
+##########################################################
+def writer1(iterator, holder, x):
+    for i, y in (iterator, holder):
+        try:
+            # game.update({f"{i}": f"{y}" })
+            game[f"{i}"] = f"{y}"
+            gameLib.append(game)
+        except:
+            pass
+    # try:
+    #     y = 1
+    #     for i in iterator:
+    #         if iterator[y]:
+
+    #             y = y + 1
+    #             game[x].update({f'exe_{y}': f'{holder[y-1]}'})
+    # except:
+    #     print('')
+    return game
+
+
+def sendtoClass(lib):
+    if not x:
+        print(str(lib))
+    else:
+        pass
+    # for i in array: 
     with open('out.txt', 'w', encoding='utf-8') as f:
-        for i in array: 
-            f.write(str(i) + '\n') 
+        for i in array:
+            f.write(str(i) + '\n')
+    with open('out.csv', 'w', encoding='utf-8') as f:
+        for i in array:
+            f.write(str(i) + '\n')
 
 
-    
 def cmd5(directory):
     VDFexe = os.path.join(directory, 'VDFparse.exe')
     out = os.path.join(directory, 'output.json')
-    
-    result = subprocess.run([VDFexe, '-h']) # , "--pretty", ">output.json"
+
+    result = subprocess.run([VDFexe, '-h'])  # , "--pretty", ">output.json"
     print()
+
 
 def cmd3():
     base_args = list()
@@ -55,9 +248,9 @@ def cmd3():
     output = '-h'
     base_args.extend([output])
     p = subprocess.Popen(' '.join(base_args),
-            shell=True,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE)
+                         shell=True,
+                         stdout=subprocess.PIPE,
+                         stderr=subprocess.PIPE)
     x = p.communicate()
     print(x)
     # 42maybe
@@ -116,13 +309,36 @@ def parse(s, t):
     return [exe, dir]
 
 
-def writer(exe, dir):
-    log = cwd + '\\output.txt'
+# def writer(exe, dir):
+#     log = cwd + '\\output.txt'
+#     with open(log, 'w', encoding='utf-8') as f:
+#         x = 0
+#         for i, p in zip(exe, dir):
+#             f.write(f'"{i}": {p},')
+#             f.write('\n') 
+
+def writer(lib, directory):
+    log = os.path.join(directory, 'output.txt')
+    csv = os.path.join(directory, 'output.csv')
+    try:
+        os.path.remove(log)
+        os.path.remove(csv)
+    except:
+        print("")
     with open(log, 'w', encoding='utf-8') as f:
-        x = 0
-        for i, p in zip(exe, dir):
-            f.write(f'"{i}": {p},')
-            f.write('\n')
+        with open(csv, 'w', encoding='utf-8') as g:
+            f.write('here are your matched paths, next update includes name. ')
+            x = 0
+            for i in matched:
+                f.write(i)
+                f.write('\n')
+                g.write(i)
+                g.write('\n')
+                # for key, val in i.items():
+                #     g.write(("\n   " + key + ': ' + val + '\n'))
+                #     g.write('\n')
+                #     f.write((key + "\n" + val + "\n"))
+                #     f.write('\n')
 
 
 # >>> s = b'your\nbyte\nstring'
@@ -140,7 +356,5 @@ def main():
     exe = l[0]
     dir = l[1]  # .split('\n')
     writer(exe, dir)
-
-
 
 # run command line args
--- call_base_arg_cmd.py
+++ call_base_arg_cmd.py
@@ -5,29 +5,56 @@ import json
 import time
 
 import subprocess
+
 directory = os.path.dirname(__file__)
 
 appinfo = r'C:\Program Files (x86)\Steam\appcache\appinfo.vdf'
 
 
-class Library():
-    # profile = data['Datasets'][400]['Data']['appinfo']
-    def __init__(self, exe, dir, path, name):
-        self.exe = exe
-        self.name = name
-        self.dir = dir
-        self.path = path
-        
+# class Library():
+#     # profile = data['Datasets'][400]['Data']['appinfo']
+#     def __init__(self, exe, dir, path, name):
+#         self.exe = exe
+#         self.name = name
+#         self.dir = dir
+#         # `self` is a reference to the current instance of the class.
+#         self.path = path
+
 def main():
-    
     list = []
-    V.moveV(directory)
-    time.sleep(0.5)
-    list = V.cmd4(directory)
-    V.parse(list)
+    if V.moveV(directory):
+
+        time.sleep(0.2)
+        list = V.cmd4(directory)
+        gamedic = V.parser(list)  # Parsing the list of games and their information.
+
+        lib = V.constructor(gamedic)
+        # A list of all the paths to the games.
+        paths = V.callLibrary(lib)
+        lib = V.pathValidator(paths, lib)
+        with open('output1.txt', 'w') as f:
+            f.write(str(lib))
+        # # Writing the data to a file.
+        V.writer(lib, directory)
+        print('\n\n\nComplete! Open the text file next to this app just generated. Find Output.txt')
+        time.sleep(5)
+    else:
+        print('\n\nClosing now. No appinfo.vdf file found.')
+
+    ########last_working#############
+    # list = []
+    # V.moveV(directory)
+    # time.sleep(0.5)
+    # list = V.cmd4(directory)
+    # gamedic = V.parser(list)
+    # val = V.constructor(gamedic)
+    # V.writer(gamedic, directory)
+    # V.writer(ar)
+    ###############################################
+
+
 # f'{VDFexe} {appinfo} --pretty >output.json'
 # 
 # # A special variable in Python that gets assigned the name of the module.
 if __name__ == "__main__":
     main()
-    
